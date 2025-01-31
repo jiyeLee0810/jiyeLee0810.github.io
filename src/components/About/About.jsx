c193bb73499534c119ca637674343801efa2cb7b
Jiye Lee <j993lee@uwaterloo.ca>
2025-01-31 17:15:36 +0900

    Cleaned up repo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,10 +4,10 @@ import styles from "./About.module.css";
 
 export const About = () => {
     return (
-        <section className={styles.container}>
+        <section id="about" className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hello, I'm Jiye</h1>
-                <p className={styles.description}>I'm a Computer Engineering student at the University of Waterloo. I love exploring innovative ways to solve real world problems. I'm currently seeking a Winter 2025 internship and would love to connect! </p>
+                <p className={styles.description}>I'm a Computer Engineering student at the University of Waterloo. I love exploring innovative ways to solve real world problems. I'm currently seeking a Fall 2025 internship and would love to connect! </p>
                 <a href="#resume" className={styles.resumeBtn}>Resume</a>
             </div>
             <img src={getImageUrl("page/pageImage.png")} alt="Image of me" className={styles.pageImg}/>
